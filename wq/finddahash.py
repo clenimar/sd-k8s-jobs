e9b1656b464810c621193f5d9bd61d2e9302ce18
Clenimar Filemon <clenimar@karamazov-apple.dyndns.cern.ch>
2017-08-22 10:42:39 +0200

    improve approach to concurrent access

--- wq/finddahash.py
+++ wq/finddahash.py
@@ -9,44 +9,48 @@ import rediswq
 # NOTE(clenimar): although kube-dns setup is working fine, the name 'redis'
 # doesn't resolve to the correct service address. use the hardcoded address
 # until I figure out what's happening.
-HOST = "10.35.250.81"
+HOST = "10.35.249.236"
 
-
-def work(prefix):
-    """Done if there is one string whose SHA256 starts with `prefix`"""
-    # create a new queue client
-    work = rediswq.RedisWQ(name="work", host=HOST)
-    print("main queue status: empty=" + str(work.empty()))
-
-    if work.empty():
-        return
-
-    # Redis is a simple key-value storage, so we're using a main queue
-    # to store the name of the queues that actually have real items to work on.
-    q_name = work.lease(lease_secs=60, block=True, timeout=2)
-    q = rediswq.RedisWQ(name=q_name.decode("utf-8"), host=HOST)
-    print("worker queue with sessionID: " + q.sessionID())
-    print("worker queue status: empty=" + str(q.empty()))
-
-
-    # now process all the items in the queue. if success, return.
+def run(prefix):
+    q = rediswq.RedisWQ(name="job-hash", host=HOST)
+    print("Worker with sessionID: " +  q.sessionID())
+    print("Initial queue state: empty=" + str(q.empty()))
     while not q.empty():
-        item = q.lease(lease_secs=10, block=True, timeout=2)
-        if item is not None:
-            string = item.decode("utf-8")
-            print(string)
-            h = sha256(string).hexdigest()
-            q.complete(item)
+        # NOTE(clenimar): this is far from ideal, but here's the deal:
+        # take a lot of strings in a bucket, then process them.
+        bucket = []
+        bucket_size = 10000000
+        for _ in range(bucket_size):
+            i = q.lease(lease_secs=1, block=False, timeout=2)
+            if item is not None:
+                bucket.append(i)
+                q.complete(i)
+            else:
+                # if we have a None item, the queue should be empty. get out.
+                break
 
-            if h.startswith(prefix):
-                print("string: %s with sha256sum %s" % (string, h))
-                return True
+        # process it!
+        print("let's find it out! bucket size: %d" % len(bucket))
+        if finddahash(prefix, bucket):
+            return True
 
-    # NOTE(clenimar): if we can't find what we're looking for, break.
-    # This way we make sure that only an instance that completes the
-    # work will finish.
-    raise Exception("job not completed.")
+
+def finddahash(prefix, bucket):
+    """Done if there is one string whose SHA256 starts with `prefix`"""
+
+    if bucket is None: raise Exception('no bucket, no sucess')
+    if not bucket: raise Exception('empty bucket, no sucess')
+
+    for i in bucket:
+        item = i.decode("utf-8")
+        h = sha256(item).hexdigest()
+        if h.startswith(prefix):
+            print ("found! string %s with sha256 %s" % (string, h))
+            return True
+
+    # if we run out of strings in our bucket, go get more strings!
+    return False
 
 
 if __name__ == '__main__':
-    work(prefix=sys.argv[1])
+    run(prefix=sys.argv[1])
