f9d8970323d9e91ab7c9fba30da5d709304d32a5
Clenimar Filemon <clenimar@karamazov.local>
2017-08-25 00:27:23 +0200

    habemus new wq approach

--- wq/finddahash.py
+++ wq/finddahash.py
@@ -1,56 +1,57 @@
+#!/usr/bin/env python
+
 from hashlib import sha256
 from random import choice, randint
-from string import ascii_lowercase
+from string import ascii_letters
+from string import digits
 import sys
 
 import rediswq
 
+LETTERS = ascii_letters + digits
+MAX_TRIES = 10000000
 
-# NOTE(clenimar): although kube-dns setup is working fine, the name 'redis'
-# doesn't resolve to the correct service address. use the hardcoded address
-# until I figure out what's happening.
-HOST = "10.35.249.236"
 
-def run(prefix):
-    q = rediswq.RedisWQ(name="job-hash", host=HOST)
-    print("Worker with sessionID: " +  q.sessionID())
-    print("Initial queue state: empty=" + str(q.empty()))
-    while not q.empty():
-        # NOTE(clenimar): this is far from ideal, but here's the deal:
-        # take a lot of strings in a bucket, then process them.
-        bucket = []
-        bucket_size = 10000000
-        for _ in range(bucket_size):
-            i = q.lease(lease_secs=1, block=False, timeout=2)
-            if item is not None:
-                bucket.append(i)
-                q.complete(i)
-            else:
-                # if we have a None item, the queue should be empty. get out.
-                break
+def check_string(string, prefix):
+    """Checks if the given string's hash starts with the given prefix."""
+    h = sha256(string).hexdigest()
+    if h.startswith(prefix):
+        print("found! string: %s \nsha256sum: %s" % (string, h))
+        return True
 
-        # process it!
-        print("let's find it out! bucket size: %d" % len(bucket))
-        if finddahash(prefix, bucket):
+
+def generate_string():
+    """Generates a random string with size between 1 and 8."""
+    return ''.join(choice(LETTERS) for _ in range(randint(1, 9)))
+
+
+def find(prefix):
+    print("looking for a hash whose prefix is %s" % prefix)
+    tries = 0
+    max_tries = MAX_TRIES
+    while tries < max_tries:
+        a = generate_string()
+        tries += 1
+        if check_string(a, prefix):
+            print("success with %d tries" % tries)
             return True
 
-
-def finddahash(prefix, bucket):
-    """Done if there is one string whose SHA256 starts with `prefix`"""
-
-    if bucket is None: raise Exception('no bucket, no sucess')
-    if not bucket: raise Exception('empty bucket, no sucess')
-
-    for i in bucket:
-        item = i.decode("utf-8")
-        h = sha256(item).hexdigest()
-        if h.startswith(prefix):
-            print ("found! string %s with sha256 %s" % (string, h))
-            return True
-
-    # if we run out of strings in our bucket, go get more strings!
     return False
 
 
-if __name__ == '__main__':
-    run(prefix=sys.argv[1])
+def run():
+    q = rediswq.RedisWQ(name="job", host="10.35.249.60")
+    print("Worker with sessionID: " +  q.sessionID())
+    print("Initial queue state: empty=" + str(q.empty()))
+    while not q.empty():
+        item = q.lease(lease_secs=40, block=True, timeout=2)
+        if item is not None:
+            prefix = item.decode("utf=8")
+            if find(prefix):
+                q.complete(item)
+        else:
+            print("Waiting for work")
+    print("Queue empty, exiting")
+
+
+run()
